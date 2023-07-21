9e8c6d6e92b84c16a67ce9e9020dbb117d86a480
TD <trongdoh@gmail.com>
2023-07-20 17:01:17 -0700

    added try catch around router.get() and router.post()

--- routes/posts.js
+++ routes/posts.js
@@ -3,24 +3,26 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 
-router.get('/', (req, res)=>{
-    res.send('We are on posts')
+router.get('/', async (req, res)=>{
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch (err){
+        res.send('We are on posts')
+    }
 });
 
-router.post('/', (req, res) =>{
+router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
-    
-    post.save()
-    .then(data =>{
-        res.json(data);
-    })
-    .catch(err =>{
-        res.json({message: err});
-    });
-
-});
+    try{
+    const savedPost = await post.save();
+    res.json(savedPost);
+    }catch(err){
+        res.json({message: err})
+    }
+})
 
 module.exports = router;
