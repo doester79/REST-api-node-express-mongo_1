eb46f2b8019eea6618fc4ba4d61dfd1c72cc51c6
TD <trongdoh@gmail.com>
2023-07-20 17:30:34 -0700

    added functions to get specific post, delete a post and update a post.

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-
+//Gets back posts
 router.get('/', async (req, res)=>{
     try{
         const posts = await Post.find();
@@ -12,17 +12,51 @@ router.get('/', async (req, res)=>{
     }
 });
 
+//sumbit a post
 router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
     try{
-    const savedPost = await post.save();
-    res.json(savedPost);
+        const savedPost = await post.save();
+        res.json(savedPost);
     }catch(err){
         res.json({message: err})
     }
 })
+
+//specific post
+router.get('/:postId', async (req, res) =>{
+    try{
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+//Delete a Post
+router.delete('/:postId', async (req, res) => {
+    try{
+        const removedPost = await Post.deleteOne({_id: req.params.postId })
+        res.json(removedPost)
+    }catch (err) {
+        res.json({ message :err });
+    }
+});
+
+//update a post
+router.patch('/:postId', async (req, res) =>{
+    try{
+        const updatePost = await Post.updateOne(
+            { _id: req.params.postId }, 
+            {$set: {title: req.body.title}}
+        );
+        res.json(updatePost)
+    }catch (err){
+        res.json ({ message: err});
+    }
+});
 
 module.exports = router;
